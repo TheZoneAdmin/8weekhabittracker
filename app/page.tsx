3c53815cd8ca70b6b3591ccaf9b854bbe20802b3
TheZoneAdmin <info@zoneneston.co.uk>
2024-11-26 15:58:32 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HabitProgram from './components/HabitProgram'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <HabitProgram />
+    </main>
+  )
+}
